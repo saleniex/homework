e1a6f707f07adad076ecaca32b82a0b205adbb9c
viljums <viljums@gmail.com>
2019-02-12 14:24:01 +0200

    Refactoring

--- Logger.php
+++ Logger.php
@@ -3,21 +3,56 @@
 
 class Logger
 {
-    public static function get()
+    private const ERROR = "ERROR";
+    private const SUCCESS = "SUCCESS";
+
+    private $logFileLocation = __DIR__. '/' . 'application_logs.log';
+
+    /**
+     * @param string|null $logFileLocation
+     */
+    public static function get($logFileLocation = null)
     {
-        return new Logger();
+        return new Logger($logFileLocation);
     }
 
+    /**
+     * If no file location provided logger writes to 
+     * __DIR__ . 'application_logs.log' 
+     * 
+     * @param string|null $logFileLocation
+     */
+    public function __construct($logFileLocation)
+    {
+        if (isset($logFileLocation)) {
+            $this->logFileLocation = logFileLocation;
+        }
+    }
+
+    /**
+     * @param string $message
+     */
     public function logError($message)
     {
-        $logFile = fopen('application.log', 'w');
-        fwrite($logFile, 'ERROR: ' . $message);
-        fclose($logFile);
+        $this->writeToFile($message, self::ERROR);
     }
 
-    public function logSuccess($msg)
+    /**
+     * @param string $message
+     */
+    public function logSuccess($message)
     {
-        $logFile = fopen('application.log', 'a');
-        fwrite($logFile, 'SUCCESS: ' . $msg);
+        $this->writeToFile($message, self::SUCCESS);
+    }
+
+    /**
+     * @param string $message
+     * @param string $status
+     */
+    private function writeToFile($message, $status)
+    {
+        $logFile = fopen($this->logFileLocation, 'a');
+        fwrite($logFile, $status. ': ' . $message . PHP_EOL);
+        fclose($logFile);
     }
 }
