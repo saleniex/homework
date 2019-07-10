db65690cdb4ae7d1e3188bb91ab21ec3474216ac
eizens <eizens@zoomcharts.com>
2019-07-10 10:23:55 +0300

    Refactor - code made more efficient and cleaner, consistant function and varaible namings, added method and class descriptions. Log file added to gitignore.

--- Logger.php
+++ Logger.php
@@ -1,23 +1,124 @@
 <?php
 
+/*
+ * Class LoggerAbstract
+ *
+ * Abstract Logger Class providing main functionality for logging messages.
+ *
+ */
 
-class Logger
-{
-    public static function get()
-    {
-        return new Logger();
+class LoggerAbstract {
+ 
+    /*
+     * Default log file location and name.
+     *
+     */
+    public $fileLocation = "application.log"; 
+
+    /* 
+     * File handler kept open 
+     * 
+     */
+    private $handler; //file handler.
+ 
+    /*
+     * Write output flag (a/w)
+     *
+     */
+    public $outputFlag = "w";
+
+
+    /*
+     * LoggerAbstract constructor.
+     *
+     */
+
+    public function __construct() {
+        $flag = $this->outputFlag;
+        $this->handler = fopen($this->fileLocation, $flag);
+    }
+ 
+    /*
+     * Method for logging success messages.
+     *
+     * @param   string  $message
+     *
+     * @return  $this
+     */
+
+    public function logError($message) {
+        $this->logMessage("error", $message);
+        return $this;
+    }
+    
+    /*
+     * Method for logging success messages.
+     *
+     * @param   string  $message
+     *
+     * @return  $this
+     */
+
+    public function logSuccess($message) {
+        $this->logMessage("success", $message);
+        return $this;
     }
 
-    public function logError($message)
-    {
-        $logFile = fopen('application.log', 'w');
-        fwrite($logFile, 'ERROR: ' . $message);
-        fclose($logFile);
+    /*
+     * Actual method that writes message to a file
+     *
+     * @param   string  $type
+     * @param   string  $message
+     *
+     * @return  $this
+     */
+
+    public function logMessage($type, $message) {
+        fwrite($this->handler, $type .': ' . $message . "\n");
+        return $this;
     }
 
-    public function logSuccess($msg)
-    {
-        $logFile = fopen('application.log', 'a');
-        fwrite($logFile, 'SUCCESS: ' . $msg);
+    /*
+     * Method for explicitly clearing log file.
+     *
+     * @return  $this
+     */
+
+    public function clearLog() {
+        $handler = fopen($this->fileLocation, 'w');
+        fclose($handler);
+        return $this;
     }
+
+}
+
+/*
+ * Class Logger
+ *
+ * Extends LoggerAbstract.  
+ *
+ */
+
+class Logger extends LoggerAbstract {
+
+    /*
+     * logger object
+     *
+     */
+
+    private static $logger;
+
+    /*
+     * Create new instance if such not already initialized;
+     *
+     * @return      object
+     */
+    
+    public static function get() {
+        if(!self::$logger) {
+            self::$logger = new Logger();
+        }
+        return self::$logger;
+    }
+
 }
