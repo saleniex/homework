6d71517573956de6ea5aacd47dca5d9f44deaf97
KKadikis <k00kadikis@gmail.com>
2019-07-21 17:16:17 +0300

    Extract repeating functionality for file logger

--- src/FileLogger.php
+++ src/FileLogger.php
@@ -4,16 +4,63 @@ namespace Homework;
 
 class FileLogger implements LoggerInterface
 {
-    public function logError($message) : void
+    const LOG_PREFIX_SUCCESS = 'SUCCESS';
+    const LOG_PREFIX_ERROR = 'ERROR';
+    const LOG_MODE_SUCCESS = 'a';
+    const LOG_MODE_ERROR = 'w';
+
+    /**
+     * @param string
+     */
+    private $logFileName;
+
+    /**
+     * @param string $logFileName
+     */
+    public function __construct(string $logFileName = 'application.log')
     {
-        $logFile = fopen('application.log', 'w');
-        fwrite($logFile, 'ERROR: ' . $message);
-        fclose($logFile);
+        $this->logFileName = $logFileName;
     }
 
-    public function logSuccess($msg) : void
+    /**
+     * Log error message to file
+     * @param string $message
+     */
+    public function logError(string $message) : void
     {
-        $logFile = fopen('application.log', 'a');
-        fwrite($logFile, 'SUCCESS: ' . $msg);
+        $this->logMessage(
+            $message,
+            self::LOG_MODE_ERROR,
+            self::LOG_PREFIX_ERROR
+        );
+    }
+
+    /**
+     * Log success message to file
+     * @param string $message
+     */
+    public function logSuccess(string $message) : void
+    {
+        $this->logMessage(
+            $message,
+            self::LOG_MODE_SUCCESS,
+            self::LOG_PREFIX_SUCCESS
+        );
+    }
+
+    /**
+     * Log string message to file in mode and with prefix passed to it
+     * @param string $message
+     * @param string $mode
+     * @param string $prefix
+     */
+    protected function logMessage(string $message, string $mode, string $prefix) : void
+    {
+        $logFile = fopen($this->logFileName, $mode);
+        fwrite(
+            $logFile,
+            sprintf("%s: %s", $prefix, $message)
+        );
+        fclose($logFile);
     }
 }
