f1e140607f183952715b6c860645e82803ff7f3d
KKadikis <k00kadikis@gmail.com>
2019-07-21 13:58:33 +0300

    Add integration tests for logging to file

---
+++ tests/LoggerTest.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+const ROOT = __DIR__ . "/../";
+
+require_once(ROOT . "Logger.php");
+
+class LoggerTest extends TestCase
+{
+    private $successMessage = 'successMessage';
+    private $errorMessage = 'errorMessage';
+    private $fullFileName = ROOT . 'application.log';
+    private $logger;
+
+    public function __construct()
+    {
+        $this->logger = Logger::get();
+
+        parent::__construct();
+    }
+
+    public function setUp() : void
+    {
+        //If the log file exists, remove it
+        if (file_exists($this->fullFileName)) {
+            unlink($this->fullFileName);
+        }
+    }
+
+    /**
+     * @group logfile
+     * @group logsuccess
+     */
+    public function testLogFileCreatedOnSuccess(): void
+    {
+        $this->logger->logSuccess($this->successMessage);
+
+        static::assertFileExists(
+            $this->fullFileName,
+            "No log file was created!"
+        );
+    }
+
+    /**
+     * @group logfile
+     * @group logsuccess
+     */
+    public function testLogContentOnSuccess(): void
+    {
+        $this->logger->logSuccess($this->successMessage);
+        $successMessagePrefix = 'SUCCESS:';
+
+        $fileContents = $this->getFileContentsIfFileExists($this->fullFileName);
+
+        static::assertStringContainsString(
+            sprintf("%s %s", $successMessagePrefix, $this->successMessage),
+            $fileContents
+        );
+    }
+
+    /**
+     * @group logfile
+     * @group logerror
+     */
+    public function testLogFileCreatedOnError(): void
+    {
+        $this->logger->logError($this->errorMessage);
+
+        static::assertFileExists(
+            $this->fullFileName,
+            "No log file was created!"
+        );
+    }
+
+    /**
+     * @group logfile
+     * @group logerror
+     */
+    public function testLogContentOnError(): void
+    {
+        $this->logger->logError($this->errorMessage);
+        $ErrorMessagePrefix = 'ERROR:';
+
+        $fileContents = $this->getFileContentsIfFileExists($this->fullFileName);
+
+        static::assertStringContainsString(
+            sprintf("%s %s", $ErrorMessagePrefix, $this->errorMessage),
+            $fileContents
+        );
+    }
+
+    /**
+     * Returns contents of file if file exists, null if it doesn't
+     * @param string $fileName
+     *
+     * @return string|null
+     */
+    protected function getFileContentsIfFileExists(string $fileName)
+    {
+        if (file_exists($fileName)) {
+            return file_get_contents($fileName);
+        }
+
+        return null;
+    }
+}
