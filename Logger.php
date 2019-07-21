3ea81f69fd77423d15241658951714a1ae77ec35
KKadikis <k00kadikis@gmail.com>
2019-07-21 20:54:38 +0300

    Add option to set logger type in environment variables

--- Logger.php
+++ Logger.php
@@ -5,11 +5,29 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Homework\LoggerInterface;
 use Homework\FileLogger;
+use Homework\ConsoleLogger;
+use Homework\Exceptions\LoggerTypeException;
 
 class Logger
 {
+    /**
+     * Return Logger depending on logger type set in environment variables
+     */
     public static function get() : LoggerInterface
     {
-        return new FileLogger();
+        $loggerType = getenv("Logger");
+        switch ($loggerType) {
+            case "false":
+            case "file":
+            case false:
+                return new FileLogger();
+                break;
+            case "console":
+                return new ConsoleLogger();
+                break;
+            default:
+                throw new LoggerTypeException("Invalid logger type supplied!");
+                break;
+        };
     }
 }
---
+++ src/Exceptions/LoggerTypeException.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Homework\Exceptions;
+
+class LoggerTypeException extends \Exception {
+}
