74efbfde54ae244b2af1e4f1f6496b14fd596fe9
Marcis Zoldners <marcis.zoldners@gmail.com>
2020-01-12 20:35:22 +0200

    Added option of switch to console output

--- Logger.php
+++ Logger.php
@@ -4,6 +4,13 @@ class Logger
 {
     protected $fileLocation = 'application.log';
 
+    protected $consoleOutput = false;
+
+    public function __construct()
+    {
+        $this->checkArguments();
+    }
+
     public static function get()
     {
         return new Logger();
@@ -11,12 +18,23 @@ class Logger
 
     public function logError($message)
     {
-        $this->writeToFile('ERROR: ' . $message);
+        $this->addRecord('ERROR', $message);
     }
 
     public function logSuccess($message)
     {
-        $this->writeToFile('SUCCESS: ' . $message);
+        $this->addRecord('SUCCESS', $message);
+    }
+
+    protected function addRecord(string $level, string $message): void
+    {
+        $record = $level . ': ' . $message . PHP_EOL;
+
+        if ($this->consoleOutput) {
+            $this->printToConsole($record);
+        } else {
+            $this->writeToFile($record);
+        }
     }
 
     protected function writeToFile(string $message): void
@@ -26,5 +44,40 @@ class Logger
         $logFile = fopen($this->fileLocation, 'a');
         fwrite($logFile, $record);
         fclose($logFile);
+    }
+
+    protected function printToConsole(string $record): void
+    {
+        echo $record;
+    }
+
+    protected function checkArguments(): void
+    {
+        $arguments = $this->getArguments();
+
+        foreach ($arguments as $argument) {
+            switch ($argument) {
+                case '--console':
+                case '--c':
+                    $this->setConsoleOutput(true);
+                    break;
+                // Place for more arguments and their methods
+            }
+        }
+    }
+
+    protected function getArguments(): array
+    {
+        $arguments = $_SERVER['argv'];
+
+        // Remove first argument because it's a file name
+        array_shift($arguments);
+
+        return $arguments;
+    }
+
+    public function setConsoleOutput(bool $status): void
+    {
+        $this->consoleOutput = $status;
     }
 }
